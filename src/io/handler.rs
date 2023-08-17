ddb5f36e4cd159787a3dc3cc2f34a669f88786d7
Keisuke Toyota <hm.pudding0715@gmail.com>
2023-08-17 23:15:35 +0900

    fix async handle event

--- src/io/handler.rs
+++ src/io/handler.rs
@@ -46,32 +46,44 @@ impl IoAsyncHandler {
     }
 
     async fn do_load_timeline(&mut self) -> Result<()> {
-        let mut app = self.app.lock().await;
-        let agent = app.state.get_agent().unwrap();
+        let agent = {
+            let app = self.app.lock().await;
+            app.state.get_agent().unwrap()
+        };
         let timeline = bsky::timeline(&agent).await?;
+        let mut app = self.app.lock().await;
         app.state.set_timeline(Some(timeline.feed));
 
         Ok(())
     }
 
     async fn do_send_post(&mut self) -> Result<()> {
+        let agent = {
+            let app = self.app.lock().await;
+            app.state.get_agent().unwrap()
+        };
+        let text = {
+            let app = self.app.lock().await;
+            app.state.get_input().value().to_string()
+        };
         {
             let mut app = self.app.lock().await;
-            let agent = app.state.get_agent().unwrap();
-            let text = app.state.get_input().value().to_string();
-            bsky::send_post(&agent, text, None).await?;
             app.state.set_mode(Mode::Normal);
             app.state.set_input(Input::default());
         }
+        bsky::send_post(&agent, text, None).await?;
         self.do_load_timeline().await?;
 
         Ok(())
     }
 
     async fn do_load_notifications(&mut self) -> Result<()> {
-        let mut app = self.app.lock().await;
-        let agent = app.state.get_agent().unwrap();
+        let agent = {
+            let app = self.app.lock().await;
+            app.state.get_agent().unwrap()
+        };
         let notifications = bsky::notifications(&agent).await?;
+        let mut app = self.app.lock().await;
         app.state
             .set_notifications(Some(notifications.notifications));
 
@@ -79,49 +91,68 @@ impl IoAsyncHandler {
     }
 
     async fn do_like(&mut self) -> Result<()> {
-        {
+        let agent = {
             let app = self.app.lock().await;
-            let agent = app.state.get_agent().unwrap();
-            let current_feed = app.state.get_current_feed().unwrap();
-            bsky::toggle_like(&agent, current_feed).await?;
-        }
+            app.state.get_agent().unwrap()
+        };
+        let current_feed = {
+            let app = self.app.lock().await;
+            app.state.get_current_feed().unwrap()
+        };
+
+        bsky::toggle_like(&agent, current_feed).await?;
         self.do_load_timeline().await?;
 
         Ok(())
     }
 
     async fn do_repost(&mut self) -> Result<()> {
-        {
+        let agent = {
             let app = self.app.lock().await;
-            let agent = app.state.get_agent().unwrap();
-            let current_feed = app.state.get_current_feed().unwrap();
-            bsky::toggle_repost(&agent, current_feed).await?;
-        }
+            app.state.get_agent().unwrap()
+        };
+        let current_feed = {
+            let app = self.app.lock().await;
+            app.state.get_current_feed().unwrap()
+        };
+
+        bsky::toggle_repost(&agent, current_feed).await?;
         self.do_load_timeline().await?;
 
         Ok(())
     }
 
     async fn do_reply(&mut self) -> Result<()> {
+        let agent = {
+            let app = self.app.lock().await;
+            app.state.get_agent().unwrap()
+        };
+        let current_feed = {
+            let app = self.app.lock().await;
+            app.state.get_current_feed().unwrap()
+        };
+        let text = {
+            let app = self.app.lock().await;
+            app.state.get_input().value().to_string()
+        };
+        let reply = ReplyRef {
+            root: strong_ref::Main {
+                cid: current_feed.post.cid.clone(),
+                uri: current_feed.post.uri.clone(),
+            },
+            parent: strong_ref::Main {
+                cid: current_feed.post.cid.clone(),
+                uri: current_feed.post.uri.clone(),
+            },
+        };
+
         {
             let mut app = self.app.lock().await;
-            let agent = app.state.get_agent().unwrap();
-            let current_feed = app.state.get_current_feed().unwrap();
-            let reply = ReplyRef {
-                root: strong_ref::Main {
-                    cid: current_feed.post.cid.clone(),
-                    uri: current_feed.post.uri.clone(),
-                },
-                parent: strong_ref::Main {
-                    cid: current_feed.post.cid.clone(),
-                    uri: current_feed.post.uri.clone(),
-                },
-            };
-            let text = app.state.get_input().value().to_string();
-            bsky::send_post(&agent, text, Some(reply)).await?;
             app.state.set_mode(Mode::Normal);
             app.state.set_input(Input::default());
         }
+
+        bsky::send_post(&agent, text, Some(reply)).await?;
         self.do_load_timeline().await?;
 
         Ok(())
